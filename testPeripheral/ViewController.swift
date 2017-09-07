8bd4d24ce6327c86f5ab9254a2aba94f46d34d5c
DesMatsue <takakura-y@macd1189.local>
2017-09-07 16:46:09 -0700

    Initial Commit

---
+++ testPeripheral/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testPeripheral
+//
+//  Created by Takakura 高倉 優介 on 2017/09/07.
+//  Copyright © 2017年 DesMatsue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
