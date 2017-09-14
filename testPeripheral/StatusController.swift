6f50dd4ef23e5ba1a5d97dd87791085de937f92b
DesMatsue <takakura-y@macd1189.local>
2017-09-14 00:43:08 -0700

    更新

--- testPeripheral/StatusController.swift
+++ testPeripheral/StatusController.swift
@@ -11,12 +11,14 @@ import UIKit
 import CoreBluetooth
 
 class StatusController: UIViewController,CBPeripheralManagerDelegate,UITextFieldDelegate {
-    var perpheralManager: CBPeripheralManager!
+    var peripheralManager: CBPeripheralManager!
     var centralManager: CBCentralManager!
+    var central:CBCentral?
     var isSending:Bool = false
     private var peripheralArray = [CBPeripheral]()
     private var serviceArray = [CBService]()
     private var charasteristicArray = [CBCharacteristic]()
+    
     @IBOutlet weak var dataUnitText: UITextField!
     @IBOutlet weak var dataIntervalUnit: UITextField!
     @IBOutlet weak var speedText: UILabel!
@@ -25,8 +27,7 @@ class StatusController: UIViewController,CBPeripheralManagerDelegate,UITextField
         super.viewDidLoad()
         dataUnitText.delegate = self
         dataIntervalUnit.delegate = self
-        
-        perpheralManager = CBPeripheralManager(delegate: self,queue: nil)
+        peripheralManager = CBPeripheralManager(delegate: self,queue: nil)
     }
     
     /*
@@ -43,6 +44,11 @@ class StatusController: UIViewController,CBPeripheralManagerDelegate,UITextField
             return
         }
         print("")
+    }
+    // invoked when remote central subscribes advertisement
+    // * 引数を変えていないことが気になる
+    func peripheralManager(_ peripheral: CBPeripheralManager, central: CBCentral, didUnsubscribeFrom characteristic: CBCharacteristic) {
+        self.central = central
     }
     
     /*
@@ -65,8 +71,36 @@ class StatusController: UIViewController,CBPeripheralManagerDelegate,UITextField
     }
     // startAdvertise
     private func startAdvertise(){
-        let advertisementData = [CBAdvertisementDataLocalNameKey: "Test Device"]
-        perpheralManager.startAdvertising(advertisementData)
+        if !isSending {
+            peripheralManager.stopAdvertising()
+        }else{
+            var arr:[UInt8] = []
+            let advertisementData = [CBAdvertisementDataLocalNameKey: "Test Device"]
+            let serviceUUID = CBUUID(string: "5736A771-18DA-4DD6-8011-9AF47D6B7C20")
+            let service = CBMutableService(type: serviceUUID, primary: true)
+            guard let interval = Int(dataIntervalUnit.text!),
+                let data = UInt8(dataUnitText.text!),
+                let cbCentral = central
+            else {
+                    // illegal input
+                    return
+            }
+            // byte配列を作成
+            for _ in 1...data {
+                arr.append(data)
+            }
+            // バイト変換
+            let submitData = Data(bytes:arr)
+            peripheralManager.setDesiredConnectionLatency(
+                CBPeripheralManagerConnectionLatency.init(rawValue: interval)!, for: cbCentral)
+            let charastericUUID = CBUUID(string: "9F392B50-34E2-453F-AE24-238FFE4CDEB5")
+            let charasteristic = CBMutableCharacteristic(
+                type: charastericUUID, properties: CBCharacteristicProperties.read,
+                value: submitData, permissions: CBAttributePermissions.readable)
+            service.characteristics = [charasteristic]
+            self.peripheralManager.add(service)
+            peripheralManager.startAdvertising(advertisementData)
+        }
     }
     
     /*
@@ -83,5 +117,6 @@ class StatusController: UIViewController,CBPeripheralManagerDelegate,UITextField
     @IBAction func submitAdvertise(_ sender: Any) {
         // Bloadcast Advertise
         isSending = !isSending
+        startAdvertise()
     }
 }
